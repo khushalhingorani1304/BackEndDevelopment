289744cc1f4cfec950657a7ae5a63da64ced8ab1
Khushal Hingorani <khushalhingorani1304@gmail.com>
2025-06-20 20:30:55 +0530

    abc

--- FileUpload/models/File.js
+++ FileUpload/models/File.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -13,6 +16,35 @@ const fileSchema = new mongoose.Schema({
     },
     email:{
         type:String
+    }
+})
+
+//^ Post Middleware
+fileSchema.post("save", async function(doc){
+    try {
+        // Transporter - ye ek agent ki tarah mail bhejega
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS
+            }
+        })
+
+
+        // Send mail and mail ka content
+        let info = await transporter.sendMail({
+            from:`EasySign`,
+            to:doc.email,
+            subject:"Important - File uploaded to cloudinary",
+            html:`<h2>Haa bhai Dalle</h2> <p>Kab aa raha hai chakkar maarne fir</p>`
+        })
+
+        console.log(info);
+        
+
+    } catch (error) {
+        console.log(error);
     }
 })
 
